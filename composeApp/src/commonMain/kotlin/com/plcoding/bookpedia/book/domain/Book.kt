92d17e826f94b98d1dfcacbe51addb1c78717dcc
Sobhan Sabeti <sobhansabeti@gmail.com>
2024-12-08 07:02:31 -0800

    commit on 42:00

---
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/domain/Book.kt
@@ -0,0 +1,17 @@
+package com.plcoding.bookpedia.book.domain
+
+import cmp_bookpedia.composeapp.generated.resources.Res
+
+data class Book(
+    val id: String,
+    val title: String,
+    val imageUrl: String,
+    val authors: List<String>,
+    val description: String,
+    val languages: List<String>,
+    val firstPublishYear: String?,
+    val averageRating: Int?,
+    val ratingCount: Int?,
+    val numPages: Int?,
+    val numEditions: Int?
+)
